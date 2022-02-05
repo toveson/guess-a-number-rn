0f191fd70a9f7e2ecaff0113cd3c74b463ddf688
Trever O <toveson9522@gmail.com>
2022-02-05 01:40:51 -0700

    set up user validation on number input and made keyboard close when another part of the screen is touched

--- components/Screens/StartGameScreen.js
+++ components/Screens/StartGameScreen.js
@@ -1,34 +1,47 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Card from '../Card';
 import Colors from '../../constants/colors';
 import Input from '../Input';
 
 const StartGameScreen = (props) => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = (inputText) => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+    };
+
     return (
-        <View style={styles.screen} >
-            <Text style={styles.title} >Start a New Game!</Text>
+        // dismisses keyboard when any other part of the screen is touched
+        <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss()}}>
+            <View style={styles.screen} >
+                <Text style={styles.title} >Start a New Game!</Text>
 
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <Input 
-                    style={styles.input}
-                    blurOnSubmit
-                    autoCapitalize='none'
-                    autoCorrect='false'
-                    maxLength={2}
-                    keyboardType='number-pad'
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input
+                        style={styles.input}
+                        // allows check to dismiss keyboard on Android
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrect='false'
+                        maxLength={2}
+                        keyboardType='number-pad'
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
                     />
-                <View style={styles.buttonContainer} >
-                    <View style={styles.button} >
-                        <Button title='Reset' onPress={() => { }} color={Colors.primary} />
+                    <View style={styles.buttonContainer} >
+                        <View style={styles.button} >
+                            <Button title='Reset' onPress={() => { }} color={Colors.primary} />
+                        </View>
+                        <View style={styles.button} >
+                            <Button title='Confirm' onPress={() => { }} color={Colors.accent} />
+                        </View>
                     </View>
-                    <View style={styles.button} >
-                        <Button title='Confirm' onPress={() => { }} color={Colors.accent} />
-                    </View>
-                </View>
-            </Card>
-
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
     )
 };
 
